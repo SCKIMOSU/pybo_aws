36839299f374201557202e55248d20971ad3aca4
Sang-Chul Kim <sckim7@kookmin.ac.kr>
2025-05-25 10:45:19 +0900

    Add files via upload

--- common/views.py
+++ common/views.py
@@ -4,25 +4,15 @@ from common.forms import UserForm
 
 
 def signup(request):
-    """
-    회원가입
-    """
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
             return redirect('index')
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
-
-
-def page_not_found(request, exception):
-    """
-    404 Page not found
-    """
-    return render(request, 'common/404.html', {})
